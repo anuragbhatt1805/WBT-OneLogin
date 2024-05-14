32936a10487a71f9e96a6c3a7c1e11993cd41a05
Anurag Bhatt <anuragbhatt1805@gmail.com>
2024-05-14 19:37:21 +0530

    chore: Update npm dependencies and add support for file uploads
    This commit updates the npm dependencies to their latest stable versions. Additionally, it adds support for file uploads by implementing the multer middleware and configuring the destination and filename for uploaded files. The file upload route '/upload' is also added to handle the file upload request and send back the uploaded file URL.

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import cors from "cors";
 
 const app = express();
 
+import path from "path"; // Import the 'path' module
+
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
@@ -12,9 +14,18 @@ app.use(
 
 app.use(express.json({ limit: "64kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10000kb" }));
-app.use(express.static("Z:/ol-data"));
+app.use(express.static("Z:/oneLogin"));
 app.use(cookieParser());
 
+import { upload } from "./middleware/multer.middleware.js";
+
+app.post('/upload', upload.fields([
+  { name: 'image', maxCount: 1 }
+]), (req, res) => {
+  const imagePath = req?.files?.image[0]?.path;
+  const imageUrl = `/uploads/${path.basename(imagePath)}`;
+  res.send(imageUrl);
+});
 
 
 
--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -3,9 +3,11 @@ import { v4 as uuidv4 } from 'uuid';
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "Z:/ol-data/uploads");
+        cb(null, "Z:/oneLogin/uploads");
     },
     filename: (req, file, cb) => {
         cb(null, `${uuidv4().toString()}.${file.originalname.split('.').pop()}`);
     },
 });
+
+export const upload = multer({ storage: storage });
