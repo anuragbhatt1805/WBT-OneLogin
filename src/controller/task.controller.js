32aa08e0a732876f9ec2f7511981736e949e81da
Anurag Bhatt <anuragbhatt1805@gmail.com>
2024-05-24 15:50:28 +0530

    refactor: Update Task controller to use findOne instead of find
    The code change in task.controller.js updates the query from Task.find to Task.findOne to retrieve a single task for the current user. This change improves efficiency by fetching only one task instead of an array of tasks, which is more appropriate for the intended functionality.

--- src/controller/task.controller.js
+++ src/controller/task.controller.js
@@ -71,7 +71,7 @@ export const getTask = asyncHandler( async (req, res) => {
         //     throw new ApiError(400, "User not verified");
         // }
 
-        const task = await Task.find({currentUser: req.user._id}).sort({priority: -1}).populate('project').populate('createdBy').populate('currentUser').populate('assign.assignedTo').populate('assign.assignedBy').populate('comments.commentedBy');
+        const task = await Task.findOne({currentUser: req.user._id}).sort({priority: -1}).populate('project').populate('createdBy').populate('currentUser').populate('assign.assignedTo').populate('assign.assignedBy').populate('comments.commentedBy');
 
         if (!task) {
             throw new ApiError(404, "Task not found");
