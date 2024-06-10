287e1f9252460803318b21e670ff452f26d63202
Anurag Bhatt <anuragbhatt1805@gmail.com>
2024-06-10 11:21:37 +0530

    refactor: Update getTask function to exclude approved tasks
    The code changes in `task.controller.js` update the `getTask` function to exclude tasks that are marked as "approved". This improvement ensures that only active and pending tasks are retrieved, providing a more accurate representation of the user's current tasks.

--- src/controller/task.controller.js
+++ src/controller/task.controller.js
@@ -73,7 +73,7 @@ export const getTask = asyncHandler( async (req, res) => {
         //     throw new ApiError(400, "User not verified");
         // }
 
-        const task = await Task.findOne({currentUser: req.user._id, status: {$ne: "complete"}}).sort({priority: -1}).populate('project').populate('createdBy').populate('currentUser').populate('assign.assignedTo').populate('assign.assignedBy').populate('comments.commentedBy');
+        const task = await Task.findOne({currentUser: req.user._id, status: {$ne: ["complete", "approved"]}}).sort({priority: -1}).populate('project').populate('createdBy').populate('currentUser').populate('assign.assignedTo').populate('assign.assignedBy').populate('comments.commentedBy');
 
         if (!task) {
             throw new ApiError(404, "Task not found");
